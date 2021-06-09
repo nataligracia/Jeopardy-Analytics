5ff4d4b7583f3967b89685302190cd43169215bc
Natali Gracia <ngracia356@gmail.com>
2021-06-09 12:27:14 -0700

    Update App

--- static/js/app.js
+++ static/js/app.js
@@ -1,71 +1,111 @@
 // from jeopardy.json
-d3.json("../data/jeopardy.json").then( function(tableData) {
+d3.json('../data/jeopardy.json').then( function(tableData = data) {
     console.log(tableData)
 
-    // get table references with d3.select()
-    const tbody = d3.select('tbody');
+// from data.js
+// const tableData = data;
 
-    // define a function called buildTable that takes an argument called data
-    function buildTable(data) {
-    // the job of this function is to parse out the data and create an html table
-    // clear out any existing data in tbody by setting the .html() to an empty string
-    tbody.html('');
+// get table references with d3.select()
+const tbody = d3.select("tbody");
 
-    // Next, loop forEach() dataRow in the data
-    // and append a row and cells for each value in the row
-    data.forEach((datarow) => {
-        // .append() a table row "tr" to the tbody
-        var row = tbody.append('tr');
+// define a function called buildTable that takes an argument called data
+function buildTable(data) {
+// the job of this function is to parse out the data and create an html table
+//YOUR_CODE_HERE
+  // clear out any existing data in tbody by setting the .html() to an empty string
+  tbody.html("");
 
-        // Loop through forEach val in the Object.values(dataRow)
-        Object.values(datarow).forEach((val) => {
-        // append each value as a table cell (td)
-        var cell = row.append('td');
-        // set the .text of cell to the val
+  // Next, loop forEach() dataRow in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // .append() a table row "tr" to the tbody
+    const row = tbody.append("tr");
+
+    // Loop through forEach val in the Object.values(dataRow)
+    Object.values(dataRow).forEach((val) => {
+      // append each value as a table cell (td)
+      let cell = row.append("td");
+      // set the .text of cell to the val
         cell.text(val);
-        });
-    });
-    };
+      }
+    );
+  });
+}
+  
+const filters = {};
+// define a function handleClick() that takes no arguments
+// the job of this function is to 
+function handleClick() {
 
-    var filters = {};
+  // Grab the #datetime value from the filter with d3.select().property()
+  const category = d3.select("#category").property("value");
+  const air_date = d3.select("#air_date").property("value");
+  const question = d3.select("#question").property("value");
+  const value = d3.select("#value").property("value");
+  const round = d3.select("#round").property("value");
+  const show = d3.select("#number").property("value");
 
-    function handleClick() {
-    // Select all "form-control" class 
-    var formControl = d3.selectAll('.form-control')
-    // console.log(formControl._groups[0]);
 
-    // Use the forEach function iterate each id from the "form-control" class, enabling the value update
-    // Use the ._groups[0] to select the group array that consists the ids
-    formControl._groups[0].forEach(function (updateElement) {
-        var updateValue = updateElement.value;
-        var filterId = updateElement.id;
-        if (updateValue) {
-        filters[filterId] = updateValue;
-        }
-        else {
-        delete filters[filterId];
-        }
-    });
-
-    // Call function to rebuild table
-    filterTable();
-    };
-
-    // Define the filterTable function
-    function filterTable () {
-        let filteredData = tableData;
+  // store the tableData into a local variable called filteredData
+  let filteredData = tableData;
     
-        Object.entries(filters).forEach(([key, value]) => {
-        filteredData = filteredData.filter(row => row[key] === value);
-        });
-    
-        buildTable(filteredData);
-    };
+  // Check to see if a date was entered and filter the
+  // data using that date.
+  if (category) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.category === category);
+  }
 
-    // Attach an event to listen for the form button #filter-btn to be clicked, it should call your handleClick function
-    d3.selectAll('#filter-btn').on('click',handleClick);
+  if (air_date) {
+    filteredData = filteredData.filter(row => row.air_date === air_date);
+  }
 
-    // Build the table with your buildTable function when the page loads
-    buildTable(tableData);
+  if (question) {
+    filteredData = filteredData.filter(row => row.question === question);
+  }
+
+  if (value) {
+    filteredData = filteredData.filter(row => row.value === value);
+  }
+
+  if (round) {
+    filteredData = filteredData.filter(row => row.round === round);
+  }
+
+  if (show) {
+    filteredData = filteredData.filter(row => row.show === number);
+  }
+
+
+//   // define a function handleClick() that takes no arguments
+//   // the job of this function is to 
+//   function handleClick() {
+  
+//     // Grab the #datetime value from the filter with d3.select().property()
+//     const date = d3.select("#datetime").property("value");
+//     // store the tableData into a local variable called filteredData
+//     let filteredData = tableData;
+  
+//     // Check to see if a date was entered and filter the
+//     // data using that date.
+//     if (date) {
+//       // Apply `filter` to the table data to only keep the
+//       // rows where the `datetime` value matches the filter value
+//       filteredData = filteredData.filter(row => row.datetime === date);
+//     }
+
+
+  // Rebuild the table by calling you buildTable() function and passing in your filteredData variable
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button #filter-btn to be clicked, it should call your handleClick function
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table with your buildTable function when the page loads
+buildTable(tableData);
 
 });
